09f4356eb710b771a14e1ecdb5bb5d1994c11498
Athul3588krishna <athul98517772@gmail.com>
2026-03-25 07:18:42 +0530

    kk

---
+++ backend/models/Customer.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const customerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true,
+    unique: true    
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Customer = mongoose.model("Customer", customerSchema);
+
+export default Customer;
